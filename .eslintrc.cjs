e2bf4466137075ab6f63c30e3c20c6c89ae64a04
Pratham das <prathamdas003@gmail.com>
2023-09-09 16:38:20 +0530

    initailized repo done

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      qoutes: 'double',
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+};
