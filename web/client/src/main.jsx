fac284664f9652ca75077c8ed640757261d4dc6a
Pratham Das <116100433+Prathamdas3@users.noreply.github.com>
2023-09-30 19:32:14 +0530

    Update main.jsx

--- web/client/src/main.jsx
+++ web/client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './assets/styles/index.css';
+import './styles/index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
